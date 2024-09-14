4fb8e4629f59dcec88efb8864c53b0ce3170da94
syedalimohsinbukhari <syedali.b@outlook.com>
2024-09-14 18:49:49 +0500

    [MINOR-12-1]
    1. minor improvements for v0.1.3

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ with open('README.md', 'r') as f:
     readme = f.read()
 
 setup(
-        name='umatrix',
-        version='0.1.3',
-        packages=find_packages(where="src"),
-        url='https://github.com/syedalimohsinbukhari/Matrices',
-        license='MIT',
-        author='Syed Ali Mohsin Bukhari, Astrophysics and Python',
-        author_email='syedali.b@outlook.com, astrophysicsandpython@gmail.com',
-        description='',
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        python_requires=">=3.9",
-        install_requires=["setuptools"],
-        include_package_data=True,
-        classifiers=[
-                "License :: OSI Approved :: MIT License",
-                "Programming Language :: Python :: 3.9",
-                "Programming Language :: Python :: 3.10",
-                "Programming Language :: Python :: 3.11"],
-        )
+    name='umatrix',
+    version='0.1.3',
+    packages=find_packages(where="src"),
+    url='https://github.com/syedalimohsinbukhari/Matrices',
+    license='MIT',
+    author='Syed Ali Mohsin Bukhari',
+    author_email='syedali.b@outlook.com',
+    description='',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    install_requires=["setuptools"],
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"],
+)
